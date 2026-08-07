8fbb1f38e593339b46361a402e89eb2f00b48e3e
sultan <sultankurbanov302@gmail.com>
2026-08-08 00:34:48 +0500

    remove and conflict exception

--- application/src/config/exeptions/exeptions_hanlder.py
+++ application/src/config/exeptions/exeptions_hanlder.py
@@ -3,7 +3,6 @@ from fastapi.responses import JSONResponse
 
 from src.config.exeptions.exeptions import (
     ApplicationException,
-    ConflictException,
     ForbiddenException,
     NotFoundException,
 )
@@ -14,10 +13,6 @@ def setup_exception_handlers(app: FastAPI) -> None:
     @app.exception_handler(NotFoundException)
     async def not_found_handler(request: Request, exc: NotFoundException):
         return JSONResponse(status_code=404, content={"error": exc.message})
-
-    @app.exception_handler(ConflictException)
-    async def conflict_handler(request: Request, exc: ConflictException):
-        return JSONResponse(status_code=409, content={"error": exc.message})
 
     @app.exception_handler(ForbiddenException)
     async def forbidden_handler(request: Request, exc: ForbiddenException):
